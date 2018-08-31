764c39e02b418769052b9fd1c19b91718617a9f9
Carlos Guevara G <caguevarag@gmail.com>
2018-08-31 17:58:29 -0500

    V0.0.4 persistencia entre vistas de la medición editada

---
+++ app/src/main/java/com/cguevarag/eheartbp/Globals.java
@@ -0,0 +1,7 @@
+package com.cguevarag.eheartbp;
+
+public class Globals {
+    static String sys = "120";
+    static String dia = "80";
+    static String pul = "66";
+}
--- app/src/main/java/com/cguevarag/eheartbp/ViewMeasurementActivity.java
+++ app/src/main/java/com/cguevarag/eheartbp/ViewMeasurementActivity.java
@@ -1,18 +1,47 @@
 package com.cguevarag.eheartbp;
 
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 
 public class ViewMeasurementActivity extends AppCompatActivity {
+
+    EditText[] fields;
+    View.OnFocusChangeListener listener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_measurement);
         overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+
+        listener = new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                validateInput(v);
+            }
+        };
+
+        fields = new EditText[3];
+        fields[0] = findViewById(R.id.editSys);
+        fields[1] = findViewById(R.id.editDia);
+        fields[2] = findViewById(R.id.editPul);
+
+        fields[0].setText(Globals.sys);
+        fields[1].setText(Globals.dia);
+        fields[2].setText(Globals.pul);
+
+        for (EditText f : fields)
+            f.setOnFocusChangeListener(listener);
+    }
+
+    private void validateInput(View v) {
+        Globals.sys = fields[0].getText().toString();
+        Globals.dia = fields[1].getText().toString();
+        Globals.pul = fields[2].getText().toString();
     }
 
     @Override
